965564c64b13dcad5ff43a26bce5a4a7fe290686
mhayescs19 <mhayescs19@gmail.com>
2021-03-15 23:16:21 -0700

    initial commit: 1) temp static home page (index.html) 2) example of additional controller (LabsController) as a way to manage route nesting

---
+++ src/main/java/com/example/demo/LabsController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/labs")
+public class LabsController {
+
+    @GetMapping("/test") // actual web address will be /labs/michael
+    public String labsMichael () {
+
+        return "labs/test";
+    }
+}
