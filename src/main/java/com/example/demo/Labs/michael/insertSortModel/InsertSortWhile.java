2a5b40e1f11b3b3706d687805fbad95a8f984baa
mhayescs19 <mhayescs19@gmail.com>
2021-04-04 16:00:32 -0700

    updated time analysis with to include relation of current execution speed with the rolling average; comments

--- src/main/java/com/example/demo/Labs/michael/insertSortModel/InsertSortWhile.java
+++ src/main/java/com/example/demo/Labs/michael/insertSortModel/InsertSortWhile.java
@@ -1,18 +1,24 @@
 package com.example.demo.Labs.michael.insertSortModel;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 public class InsertSortWhile extends _InsertionSort {
-
+    /* Statics for time analysis */
     private static List<Integer> speedRecords = new ArrayList<>();
-    private static int rollingAverageSpeed = 1000;
+    private static int rollingAverageSpeed;
+    private static int timeComparedToAverage;
+
 
     public InsertSortWhile(String referenceArrayLiteral) {
         super(referenceArrayLiteral);
 
         this.speedRecords.add(this.getProcessTimeNano());
-        rollingAverageSpeed = (rollingAverageSpeed + this.getProcessTimeNano()) / 2;
+
+        staticInit();
     }
 
 
@@ -40,6 +46,7 @@ public class InsertSortWhile extends _InsertionSort {
     }
 
     public int getLastSpeedRecord() {
+        /* Index out of bounds exception avoidance via simple logic */
         if (speedRecords.size() <= 1) {
             return speedRecords.get(speedRecords.size() - 1);
         } else {
@@ -47,7 +54,24 @@ public class InsertSortWhile extends _InsertionSort {
         }
     }
 
+    /* Static getters applicable to purpose of statics (used in th HTML) */
     public int getRollingAverageSpeed() { return rollingAverageSpeed; }
+
+    public int getTimeComparedToAverage() { return timeComparedToAverage;}
+
+    /**
+     * Uses speed records created for each activation and creates a rolling average
+     * and comparison to rolling average
+     */
+    public void staticInit() {
+        if (speedRecords.size() <= 1){
+            rollingAverageSpeed = this.getProcessTimeNano();
+        } else {
+            rollingAverageSpeed = (rollingAverageSpeed + this.getProcessTimeNano()) / 2;
+        }
+
+        timeComparedToAverage = rollingAverageSpeed - this.getProcessTimeNano();
+    }
 
     public static void main(String[] args){
         String referenceArrayLiteral = "7,6,3,1,9,2";
