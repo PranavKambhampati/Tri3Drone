1dff278a1b13fdd54a3da44829547335c9f532c5
mhayescs19 <mhayescs19@gmail.com>
2021-04-24 23:06:00 -0700

    POJO test data implemented

--- src/main/java/com/example/demo/Labs/michael/SortsController.java
+++ src/main/java/com/example/demo/Labs/michael/SortsController.java
@@ -4,6 +4,10 @@ import com.example.demo.Labs.michael.sortsModel.BubbleSort;
 import com.example.demo.Labs.michael.sortsModel.InsertionSort;
 import com.example.demo.Labs.michael.sortsModel.SelectionSort;
 import com.example.demo.Labs.michael.sortsModel._Sorts;
+import com.example.demo.Labs.michael.trackModel.Athlete;
+import com.example.demo.Labs.michael.trackModel.BasicData;
+import com.example.demo.Labs.michael.trackModel.Distance;
+import com.example.demo.Labs.michael.trackModel.Entries;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Controller;
@@ -18,13 +22,29 @@ import java.util.List;
 @Controller
 public class SortsController {
     List<_Sorts> sortsList;
+    Entries pojoData;
+
+    private Distance.KeyType distanceKey;
+
 
     public void init(){
+        /* Integer and String data */
         sortsList = new ArrayList<>();
 
         sortsList.add(new BubbleSort("10,2,6,1,7,5"));
         sortsList.add(new InsertionSort("10,2,6,1,7,5"));
         sortsList.add(new SelectionSort("10,2,6,1,7,5"));
+
+        sortsList.add(new BubbleSort("g,y,e,w,v,b,a"));
+        sortsList.add(new InsertionSort("g,y,e,w,v,b,a"));
+        sortsList.add(new SelectionSort("g,y,e,w,v,b,a"));
+
+
+        /* Java POJO Athlete */
+        this.distanceKey = Distance.KeyType.name;
+
+        pojoData = new Entries();
+        this.addPOJO(Distance.distanceAthleteData());
     }
 
     @GetMapping("/michaelSorts")
@@ -34,5 +54,11 @@ public class SortsController {
 
         model.addAttribute("sorts", this);
         return "labs/michaelSorts";
+    }
+
+    public void addPOJO(BasicData[] array) {
+        for (BasicData data : array) {
+            this.pojoData.addEntry(data);
+        }
     }
 }
