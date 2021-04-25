2db1f744fa3f3f97adaa0e2a6684927c24d10aac
mhayescs19 <mhayescs19@gmail.com>
2021-04-25 16:49:03 -0700

    fix for whitelabel EC2 error

---
+++ src/main/java/com/example/demo/Labs/michael/trackModel/extras/AthleteNames.java
@@ -0,0 +1,39 @@
+package com.example.demo.Labs.michael.trackModel.extras;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AthleteNames {
+    public List<String> names;
+
+    public AthleteNames() {
+        names = new ArrayList<>();
+
+        names.add("Vincent Hughes");
+        names.add("Eric Compton");
+        names.add("David Andrade");
+        names.add("Christopher Ellison");
+        names.add("John Garcia");
+        names.add("Ralph Taylor");
+        names.add("Bradley Richardson");
+        names.add("Edward Gonzalez");
+        names.add("John Haas V");
+        names.add("David Santana");
+        names.add("Corey Smith");
+        names.add("Brian May");
+        names.add("Todd Morgan");
+        names.add("Samuel Pham");
+        names.add("Leroy O'Donnell");
+        names.add("John Tyler");
+        names.add("Daniel Harrison");
+        names.add("Aaron Scott");
+        names.add("Mark Jackson");
+        names.add("Alex Barnes");
+        names.add("Evan Figueroa");
+        names.add("Barry Wood");
+        names.add("Jackson Neal");
+        names.add("Darren Fowler");
+
+
+    }
+}
