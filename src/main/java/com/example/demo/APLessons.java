44b37f9fa8afd023db08385645ed075a447dcc63
PranavKambhampati <pranav.21950@gmail.com>
2021-04-26 08:46:50 -0700

    Aryan and Pranav's Presentations of AP Lessons added to website under "AP Lessons"

--- src/main/java/com/example/demo/APLessons.java
+++ src/main/java/com/example/demo/APLessons.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/ap-lessons")
 public class APLessons {
+
+    @GetMapping("/lessons") // actual web address will be /ap-lessons/lessons;
+    public String PranavLesson () {
+        return "labs/APpresentations";
+    }
 
 }
