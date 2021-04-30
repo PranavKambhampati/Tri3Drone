927ccc4ec2d3cd1aefadd14cfeff1a5979d5faf4
aryan114 <amberkararyan@gmail.com>
2021-04-30 16:56:17 -0700

    Added bubble sort method for sorting array lists

--- src/main/java/com/example/demo/Labs/AryanLinkedLists/AryanArrayLists.java
+++ src/main/java/com/example/demo/Labs/AryanLinkedLists/AryanArrayLists.java
@@ -1,10 +1,12 @@
 package com.example.demo.Labs.AryanLinkedLists;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AryanArrayLists {
 
-    public ArrayList addingArrayList (Object[] objects) {
+    public ArrayList<Object> addingArrayList (Object[] objects) {
         int length = objects.length;
         ArrayList objects1 = new ArrayList<Object>(length);
         int number = 0;
@@ -13,6 +15,22 @@ public class AryanArrayLists {
             number++;
         }
         return objects1;
+    }
+
+    public ArrayList<Object> BubbleSort (Object[] objects) {
+        int length = objects.length;
+        for (int i = 0; i < length - 1; i++) {
+            for (int k = 0; k < length - i - 1; k++) {
+
+                if (objects[k].toString().compareTo(objects[k+1].toString()) > 0) {
+                    Object temporaryIndex = objects[k];
+                    objects[k] = objects[k+1];
+                    objects[k+1] = temporaryIndex;
+                }
+            }
+        }
+        List<Object> arrayIntoObjects = Arrays.asList(objects);
+        return (ArrayList<Object>) arrayIntoObjects;
     }
 
 
