1f4302a34da9b1988922902d2e08da3c95cef38b
mhayescs19 <mhayescs19@gmail.com>
2021-05-10 22:40:42 -0700

    changed code embed to in house GitHub Gist for interface frq and inheritance demo

--- src/main/java/com/example/demo/lessons/inheritance/Administrator.java
+++ src/main/java/com/example/demo/lessons/inheritance/Administrator.java
@@ -1,6 +1,6 @@
 package com.example.demo.lessons.inheritance;
 
-public class Administrator extends Employee{
+public class Administrator extends Employee {
     private String scope;
 
     public Administrator(String name, int age, String scope) {
