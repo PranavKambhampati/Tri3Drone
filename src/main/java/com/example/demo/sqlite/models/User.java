8d0cd14d6208dd166216fd311f6080bbadc98e99
mhayescs19 <mhayescs19@gmail.com>
2021-05-20 08:57:09 -0700

    user POJO and sql and jpa

--- src/main/java/com/example/demo/sqlite/models/User.java
+++ src/main/java/com/example/demo/sqlite/models/User.java
@@ -1,5 +1,6 @@
 package com.example.demo.sqlite.models;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import javax.persistence.Id;
@@ -27,12 +28,12 @@ public class User {
     @Size(min=2, max=30): Allows names between 2 and 30 characters long.
      */
     @NonNull
-    //@Size(min = 2, max = 30, message = "Name (2 to 30 chars)")
+    @Size(min = 2, max = 30, message = "Name (2 to 30 chars)")
     private String name;
 
     // Age is restricted
     @NonNull
-    //@Min(0) @Max(122)
+    @Min(0) @Max(122)
     private Integer age;
 
 }
