01aa0970798e03ab0813d61515e43808d93954f8
Nakul Nandhakumar <nakul@nandhakumar.net>
2021-05-31 14:30:16 -0700

    Started Create User

---
+++ src/main/java/com/example/demo/Security/NewUser.java
@@ -0,0 +1,90 @@
+package com.example.demo.Security;
+
+
+import javax.validation.constraints.*;
+
+
+/**
+ * User that represents a new user from the create user page.
+ * This Is a DTO
+ */
+public class NewUser {
+    @NotBlank(message = "Name cannot be null")
+    @NotNull(message = "Name cannot be null")
+    @Size(min = 3, max = 20, message = "name must be bigger than 3 characters and must be less than 20")
+    private String name;
+    @NotNull(message = "Username must be filled in")
+    @NotBlank(message = "Username must not be blank")
+    @Size(min = 4, max=12, message="username must be greater then 4 digits long and less than 12 digits")
+    private String username;
+    @NotBlank(message = "password may not be empty")
+    @NotNull(message = "Password may not be empty")
+    private String password;
+    @NotNull
+    @Min(value = 13,message = "Age must be greater than 13 or equal to")
+    @Max(value = 20,message = "Age must be less than 20 or equal to")
+    private int age;
+    @NotBlank(message = "Field must be filled in")
+    @NotNull(message = "Field must be filled in")
+    private String year;
+    private final String Role;
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRole() {
+        return Role;
+    }
+
+    public NewUser() {
+        this.Role = "STUDENT";
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    @Override
+    public String toString() {
+        return "NewUser{" +
+                "name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", Age=" + age +
+                ", year='" + year + '\'' +
+                ", Role='" + Role + '\'' +
+                '}';
+    }
+
+}
---
+++ src/main/java/com/example/demo/Security/SecurityController.java
@@ -0,0 +1,47 @@
+package com.example.demo.Security;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class SecurityController {
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @GetMapping("/login")
+    public String getLoginView() {
+        return "security/login";
+    }
+
+    @GetMapping("/logout")
+    public String getLogoutView() {
+        return "security/logout";
+    }
+
+    @GetMapping("/createUser")
+    public String createUser(Model model)
+    {
+        model.addAttribute("newUser", new NewUser());
+        return "security/createUser";
+    }
+
+    @PostMapping("/createUser")
+    public String createUser(@Valid NewUser user, BindingResult bindingResult) throws JsonProcessingException {
+
+        if (bindingResult.hasErrors()) {
+            return "security/createUser";
+        }
+
+        user.setPassword(encoder.encode(user.getPassword()));
+        return "security/successful";
+    }
+}
